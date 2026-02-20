72e2a5c24ade03f7cf5423653cfe4d82aebbfe4d
Kyle Porter <kyle@5090-Gaming.localdomain>
2026-02-20 22:01:13 +0900

    initial layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import AOS from "aos"
+import "aos/dist/aos.css"
+
+AOS.init()
