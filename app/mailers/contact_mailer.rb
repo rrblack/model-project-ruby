245dc4ede0b081644a8dfa9dc39d95466a6c8021
Kyle Porter <kyle@5090-Gaming.localdomain>
2026-02-21 21:59:19 +0900

    added cover photo, contact page

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,8 @@
 class ContactMailer < ApplicationMailer
+  def contact_email(name, email, message)
+    @name = name
+    @email = email
+    @message = message
+    mail(to: "kyle@kylesjapan.life", subject: "Contact form from #{@name}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "main#index"
+
+ # Contact
   get "/contact", to: "main#contact"
   post "/contact", to: "main#submit_contact"
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
