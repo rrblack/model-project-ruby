2be1b07cbd734439aa4f1715777d8a43bba4567f
Kyle Porter <okaneookii@gmail.com>
2026-03-24 14:32:57 +0900

    added slideshow function and pictures as well as voice lines

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -3,5 +3,20 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="slideshow"
 export default class extends Controller {
   connect() {
+    this.slides = this.element.querySelectorAll(".slide")
+    this.current = 0
+    this.timer = setInterval(() => this.advance(), 7500 )
+  }
+  advance(){
+    const outgoing = this.slides[this.current]
+    const computed = getComputedStyle(outgoing).transform
+    outgoing.style.transform = computed
+    outgoing.classList.remove("active")
+    this.current = (this.current + 1) % this.slides.length
+    this.slides[this.current].style.transform = "" 
+    this.slides[this.current].classList.add("active")
+  }
+  disconnect(){
+    clearInterval(this.timer)
   }
 }
