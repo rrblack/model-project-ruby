a2a745eecd9275ce94007a86292b8475400578ac
Kyle Porter <okaneookii@gmail.com>
2026-04-01 11:07:13 +0900

    added whole photo gallery (will it upload)?

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -3,26 +3,28 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="gallery"
 export default class extends Controller {
   static targets = ["panel"]
+
   connect() {
     this.panelTargets.forEach(panel => {
       if (panel.dataset.category === "koukoku") {
-        panel.classList.remove("opacity-0")
+        panel.classList.remove("opacity-0", "h-0", "overflow-hidden", "pointer-events-none")
         panel.classList.add("opacity-100")
       } else {
         panel.classList.remove("opacity-100")
-        panel.classList.add("opacity-0")
+        panel.classList.add("opacity-0", "h-0", "overflow-hidden", "pointer-events-none")
       }
     })
   }
-  show(e){
+
+  show(e) {
     const category = e.currentTarget.dataset.category
     this.panelTargets.forEach(panel => {
       if (panel.dataset.category === category) {
-        panel.classList.remove("opacity-0")
+        panel.classList.remove("opacity-0", "h-0", "overflow-hidden", "pointer-events-none")
         panel.classList.add("opacity-100")
       } else {
         panel.classList.remove("opacity-100")
-        panel.classList.add("opacity-0")
+        panel.classList.add("opacity-0", "h-0", "overflow-hidden", "pointer-events-none")
       }
     })
   }
